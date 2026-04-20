81b4f768164e0162dc4ee689a555ec188cffe172
WZ2100TankerSU <119127545+therevtanker2100@users.noreply.github.com>
2026-04-20 12:29:19 -0500

    Forgot...

--- scripts/build_ipa.sh
+++ scripts/build_ipa.sh
@@ -12,7 +12,7 @@ xcodebuild \
   -scheme lara \
   -configuration Debug \
   -sdk iphoneos \
-  -arch arm64e \
+  -arch arm64 \
   CODE_SIGNING_ALLOWED=NO \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY="" \
